e0592d3b936f2d91906844b46a11488abed45475
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-02-05 11:16:00 +0530

    Arduino car 3

---
+++ Car_3.ino
@@ -0,0 +1,24 @@
+int in1 = 11;
+int in2 = 10;
+int in3 = 9;
+int in4 = 8;
+int enA = 6;
+int enB = 5;
+
+char x;
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+    pinMode(enA, OUTPUT);
+    pinMode(enB, OUTPUT);
+}
+
+void loop()
+{
+}
